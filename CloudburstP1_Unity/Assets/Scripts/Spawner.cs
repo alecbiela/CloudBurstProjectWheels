745b504db613b1694ec7ab1484c6c7a924bc51a4
cbrown3 <cbrow2014@gmail.com>
2016-11-07 23:55:26 -0500

    Added Spawner
    Spawner spawns enemies in intervals if it is within a certain distance

---
+++ CloudburstP1_Unity/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    
+    public Player p1;
+    public Enemy e;
+    private float time;
+	// Use this for initialization
+	void Start () {
+        time = 5.0f;
+        Debug.Log("Initialized!");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time -= Time.deltaTime;
+
+        if(Vector3.Distance(transform.position.normalized, p1.transform.position.normalized) <= 15.0f)
+        {
+            Debug.Log("Within Distance");
+            if (time < 0)
+            {
+                Debug.Log("Spawned!");
+                Instantiate(e);
+                e.position = transform.position;
+                time = 5.0f;
+            }
+        }
+	}
+}
