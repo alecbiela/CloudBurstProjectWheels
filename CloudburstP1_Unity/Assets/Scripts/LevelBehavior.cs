6c582f454b6efed4db7206178de46707655c4f80
Niko Bazos <ndb9897@rit.edu>
2016-11-12 18:53:33 -0500

    Scaled tthe player up, shortened the wall jump, and started to implement vertical moving platforms. Right now the platform moves up then teleports to a position far under its current and then proceeds to move up again. It will be streamlined and working soon.

---
+++ CloudburstP1_Unity/Assets/Scripts/LevelBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class LevelBehavior : MonoBehaviour 
+{
+	//Attributes
+	public GameObject verticalPlatform;
+	public GameObject referenceObject;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		MovingPlatformVertically();
+	
+	}
+
+
+	void MovingPlatformVertically()
+	{
+		if (verticalPlatform.transform.position.y < (referenceObject.transform.position.y + 100.0f)) 
+		{
+			verticalPlatform.transform.position = new Vector3 (verticalPlatform.transform.position.x, verticalPlatform.transform.position.y + 5.0f *Time.deltaTime);
+		} 
+		else if (verticalPlatform.transform.position.y > (referenceObject.transform.position.y + 100.0f)) 
+		{
+			while(verticalPlatform.transform.position.y > (referenceObject.transform.position.y - 100.0f))
+			{
+				verticalPlatform.transform.position = new Vector3 (verticalPlatform.transform.position.x, verticalPlatform.transform.position.y - 5.0f *Time.deltaTime);
+			}
+		} 
+	}
+}
